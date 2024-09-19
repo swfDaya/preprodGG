f99735e19525f34abb3222fc984ae08ce19ad254
swfDaya <dayanidhi1997n@gmail.com>
2024-09-19 12:39:17 +0530

    sidebar changes

--- src/ForApp/Sidebar.tsx
+++ src/ForApp/Sidebar.tsx
@@ -1,13 +1,13 @@
 import './Sidebar.css'
 
-// import logo from './assets/logo.svg'
-// import briefcase from './assets/briefcase.svg'
+import logo from './assets/logo.svg'
+import briefcase from './assets/briefcase.svg'
 // import chevronsLeft from './assets/chevrons-left.svg'
 import arrowLeft from './assets/arrow-left-circle.svg'
 import arrowRight from './assets/arrow-right-circle.svg'
-// import cloudDrizzle from './assets/cloud-drizzle.svg'
-// import film from './assets/film.svg'
-// import headphones from './assets/headphones.svg'
+import cloudDrizzle from './assets/cloud-drizzle.svg'
+import film from './assets/film.svg'
+import headphones from './assets/headphones.svg'
 
 interface SidebarProps {
   isExpanded: boolean;
@@ -23,26 +23,71 @@ const Sidebar = ({ isExpanded, setIsExpanded }: SidebarProps) => {
             <div
             className='app__sidebar__logo'
             >
-
+                <img src={logo} alt='logo' />
+                <div
+                className={`app__sidebar__logo__text ${isExpanded ? 'expanded' : ''}`}
+                >
+                    Gains Gauge
+                </div>
             </div>
             <div
             className='app__sidebar__dashboard'
             >
-
+                <img src={briefcase} alt='briefcase' />
+                <div
+                className={`app__sidebar__logo__text ${isExpanded ? 'expanded' : ''}`}
+                >
+                    Briefcase
+                </div>
+            </div>
+            <div
+            className='app__sidebar__cloud__drizzle'
+            >
+                <img src={cloudDrizzle} alt='cloud-drizzle' />
+                <div
+                className={`app__sidebar__logo__text ${isExpanded ? 'expanded' : ''}`}
+                >
+                    Cloud Drizzle
+                </div>
+            </div>
+            <div
+            className='app__sidebar__film'
+            >
+                <img src={film} alt='film' />
+                <div
+                className={`app__sidebar__logo__text ${isExpanded ? 'expanded' : ''}`}
+                >
+                    Film
+                </div>
+            </div>
+            <div
+            className='app__sidebar__headphones'
+            >
+                <img src={headphones} alt='headphones' />
+                <div
+                className={`app__sidebar__logo__text ${isExpanded ? 'expanded' : ''}`}
+                >
+                    Headphones
+                </div>
             </div>
             <div
             className='app__sidebar__spacer'
             >
-
+                
             </div>
             <div
             className='app__sidebar__menu__collapse'
+            onClick={() => setIsExpanded(!isExpanded)}
             >
                 <img 
                 src={isExpanded ? arrowLeft : arrowRight}
                 alt='menu__collapse__icon' 
-                onClick={() => setIsExpanded(!isExpanded)}
                 />
+                <div
+                className={`app__sidebar__collapse__text ${isExpanded ? 'expanded' : ''}`}
+                >
+                    Hide Menu
+                </div>
             </div>
         </div>
     )
